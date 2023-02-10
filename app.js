5c46787e4d6ba850dcb3c95990de5b698f5f525f
Dev-badol <devbadol669@gmail.com>
2023-02-11 01:14:36 +0600

    done

---
+++ app.js
@@ -0,0 +1,15 @@
+const student = {
+    name: "badol",
+    id:121,
+    address:"saver",
+    friends:["badol","khan","rana"],
+    car:{
+        brand:"tesla",
+        prices:4000,
+        made:2025,
+       menufa: {
+        bang:"bangladesh"
+       }
+    }
+}
+console.log(student.car.menufa)

bb200c1d31a4ad0b7434b2b554f04e089a7b941d
Dev-badol <devbadol669@gmail.com>
2023-02-11 01:27:35 +0600

    find a product in array of object

--- app.js
+++ app.js
@@ -20,3 +20,29 @@ const numbers = [45,11,22,33,5,85,44]
 for(const number of numbers){
     console.log(number)
 }
+const products = [
+    {id:1,name:"redmi",price:1400},
+    {id:2,name:"samsung",price:1500},
+    {id:3,name:"iphone",price:1600},
+    {id:4,name:"nokea",price:1700},
+    {id:5,name:"realme",price:1800},
+    {id:6,name:"itel",price:1900},
+]
+// for(const product of products){
+//     console.log(product)
+// }
+
+// find a array of object in function
+
+function matchedProducts (product , search){
+    const matched = []
+    for(const product of products){
+       if(product.name.includes("redmi")){
+        matched.push(product)
+       }
+    }
+    return matched
+}
+
+const result = matchedProducts(products,"redmi")
+console.log(result)
